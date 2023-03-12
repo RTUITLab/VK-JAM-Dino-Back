b03d3572639b757e4c082a6ed67d8c2413a45110
IvLaptev <ivlaptev13@ya.ru>
2023-03-12 12:09:07 +0300

    Create room join

--- app/endpoints/run_session.py
+++ app/endpoints/run_session.py
@@ -1,13 +1,43 @@
 from fastapi import APIRouter, WebSocket
+from uuid import uuid1
+from random import Random
 
+
+current_room = uuid1()
+room_size = 2
+rand = Random(300)
 
 runs = dict()
+runs[current_room] = []
+
+connections = dict()
 
 run_router = APIRouter()
 
+
+async def start_run(room_id):
+    for user in runs[room_id]:
+        await connections[user["conn"]]["conn"].send_json({"action": "start", "body": {"seed": rand.randint(1, 900)}})
+
+
 @run_router.websocket('/run')
-async def connect_to_run(socket: WebSocket):
-    await socket.accept()
-    while True:
-        data = await socket.receive_json()
-        await socket.send_text(f"Message text was: {data}")
+async def connect_to_run(connection: WebSocket):
+    global current_room
+    await connection.accept()
+    connection_id = uuid1()
+    connections[connection_id] = {"conn": connection, "room": current_room}
+
+    runs[current_room].append(
+        {"conn": connection_id, "score": 0, "alive": True})
+    if (len(runs[current_room]) >= room_size):
+        await start_run(current_room)
+        current_room = uuid1()
+
+    try:
+        while True:
+            data = await connection.receive_text()
+
+        #     for i in range(len(runs['1'])):
+        #         await runs['1'][i].send_text(f"Message text was: {data}")
+    except Exception as e:
+        pass
